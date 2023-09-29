a08546b8d643afc538b630bc7b871b46e213946a
DJDAYDAY <3628684739@qq.com>
2023-09-29 14:45:08 +0800

    写了layernorm和gelu的替换，求你了哥，给我点活干吧

--- autotrans_spec_1.0/builder.py
+++ autotrans_spec_1.0/builder.py
@@ -23,14 +23,15 @@ def main():#
             # optypes in dag_match:  Identity, Constant, Shape, Expand, Gather, Add, Layernorm, Mul, MatMul, 
             #                        Reshape, Transpose, Div, Softmax, Gelu, Gemm, Tanh, Relu
             # seemingly not found
-            dimension=content_list[i]["input_size"][0][0]
+            dimension_1=content_list[i]["input_size"][0][0]
+            dimension_2=content_list[i]["input_size"][0][1]
             if(optype == 'gelu'):
                 modified_gelu=open("./modified_verilog/nnlut_gelu.v","r+")
                 with open("./autotrans_spec_1.0/op_trans/nnlut_gelu_64.v","r+") as gelufile:
                     data_lines=gelufile.readlines()
                     for line in data_lines:
                         if 'parameter DIMENTION = 64' in line:
-                            modified_gelu.write(line.replace('parameter DIMENTION = 64', 'parameter DIMENTION = {num}'.format(num=dimension)))
+                            modified_gelu.write(line.replace('parameter DIMENTION = 64', 'parameter DIMENTION = {num}'.format(num=dimension_1)))
                         else:
                             modified_gelu.write(line)
 
@@ -44,7 +45,15 @@ def main():#
 
             # elif(optype == 'MADD'):
 
-            # elif(optype == 'layernorm'):
+            elif(optype == 'layernorm'):
+                modified_layernorm=open("./modified_verilog/nnlut_layernorm.v","r+")
+                with open("./autotrans_spec_1.0/op_trans/layernorm_nnlut.v","r+") as layernormfile:
+                    data_lines=layernormfile.readlines()
+                    for line in data_lines:
+                        if 'parameter   INPUT_NUM = 768' in line:
+                            modified_layernorm.write(line.replace('parameter   INPUT_NUM = 768,', 'parameter INPUT_NUM = {num}'.format(num=dimension_2)))
+                        else:
+                            modified_layernorm.write(line)
 
             
             # elif(optype == 'MM'):
